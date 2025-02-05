10d870349b26daae53fdda15b3e5d0869cbd0057
Manish Jha Kumar <jmanish@CEIIND.COM>
2025-02-05 17:54:51 +0530

    added seperate file

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function addtext(event) {
+    event.preventDefault();
+    var mes = document.getElementById("ip-mess").value;
+   var add = document.getElementById("chat-body").innerHTML = mes;
+   
+  
+}
